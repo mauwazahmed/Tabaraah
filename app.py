dd9cf8684aaa03e626660b1beef3a765dcfed98f
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2025-04-10 20:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat):
+def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat, itar):
     prices = {
-        "Dates": {"Ajwa": 2, "Kalmi": 2, "Sukri": 1, "Medjool": 3},
-        "Tasbih" : {"Type 1": 8, "Type 2": 10, "Type 3": 12, "Type 4": 14},
-        "Miswak" : {"Type 1": 8, "Type 2": 10, "Type 3": 12, "Type 4": 14},
-        "Topi" : {"Type 1": 10, "Type 2": 15, "Type 3": 20, "Type 4": 25},
-        "ZamZam" : {"100 ml":15, "50 ml" : 10},
-        "Mat" : {"Type 1": 120, "Type 2": 150, "Type 3": 200, "Type 4": 250}
+        "Dates": {"Ajwa": 6, "Kalmi": 6, "Sukri": 6, "Mabroom": 4},
+        "Tasbih" : {"Type 1": 10, "Type 2": 15, "Type 3": 17, "Type 4": 20},
+        "Miswak" : {"Type 1": 10, "Type 2": 15},
+        "Topi" : {"Type 1": 15, "Type 2": 17, "Type 3": 20, "Type 4": 25},
+        "ZamZam" : {"Pink 100ml":6, "Pink 60ml" : 5, "Green 60ml" : 2.5},
+        "Mat" : {"Type 1": 120, "Type 2": 150, "Type 3": 195, "Type 4": 250},
+        "Itar" : {"Type 1": 10, "Type 2": 15, "Type 3": 20}
     }
     
     total_cost = 0
@@ -24,6 +25,8 @@ def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat):
         total_cost += prices["ZamZam"][item] * detail[0] * detail[1]
     for item, detail in mat.items():
         total_cost += prices["Mat"][item] * detail[0] * detail[1]
+    for item, detail in itar.items():
+        total_cost += prices["Itar"][item] * detail[0] * detail[1]
     
     packaging = 10
     labor = 5
@@ -36,30 +39,35 @@ def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat):
 
 st.title("Hajj/Umrah Gift Cost Calculator")
 st.divider()
+
 data = {
     "ZamZam" : {
-        "Type": ["50 ml", "100 ml"],
-        "Price per Unit (Rs)": [10, 15]
+        "Type": ["Pink 100ml", "Pink 60ml", "Green 60ml"],
+        "Price per Unit (Rs)": [6, 5, 2.5]
     },
     "Dates" : {
-        "Type": [ "Ajwa", "Kalmi", "Sukri", "Medjool"],
-        "Price per Unit (Rs)": [2, 2, 1, 3]
+        "Type": [ "Ajwa", "Kalmi", "Sukri", "Mabroom"],
+        "Price per Unit (Rs)": [6, 6, 6, 4]
     },
     "Tasbih" : {
         "Type":  ["Type 1", "Type 2", "Type 3", "Type 4"],
-        "Price per Unit (Rs)": [8, 10, 12, 14]
+        "Price per Unit (Rs)": [10, 15, 17, 20]
     },
     "Miswak" : {
-        "Type": ["Type 1", "Type 2", "Type 3", "Type 4"],
-        "Price per Unit (Rs)": [8, 10, 12, 14]
+        "Type": ["Type 1", "Type 2"],
+        "Price per Unit (Rs)": [10, 15]
     },
     "Topi" : {
         "Type": ["Type 1", "Type 2", "Type 3", "Type 4"],
-        "Price per Unit (Rs)": [10, 15, 20, 25]
+        "Price per Unit (Rs)": [15, 17, 20, 25]
     },
     "Mat" : {
         "Type": ["Type 1", "Type 2", "Type 3", "Type 4"],
         "Price per Unit (Rs)": [120, 150, 200, 250]
+    },
+    "Itar" : {
+        "Type": ["Type 1", "Type 2", "Type 3"],
+        "Price per Unit (Rs)": [10, 15, 20]
     }
 }
     
@@ -72,43 +80,48 @@ st.divider()
 df = pd.DataFrame(data["ZamZam"])
 st.subheader("ZamZam")
 st.dataframe(df,hide_index=True)
-zamzam = {st.selectbox("Select Zamzam Bottle Quantity", ["50 ml", "100 ml"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=2),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key = 3)]}
+zamzam = {st.selectbox("Select Zamzam Bottle Quantity", data["ZamZam"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=2),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key = 3)]}
 st.divider()    
 
 
 df = pd.DataFrame(data["Dates"])
 st.subheader("Dates")
 st.dataframe(df,hide_index=True)
-dates = {st.selectbox("Select Date Type", ["Ajwa", "Kalmi", "Sukri", "Medjool"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=4),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets,key=5)]}
+dates = {st.selectbox("Select Date Type", data["Dates"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=4),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets,key=5)]}
 st.divider()    
 
 df = pd.DataFrame(data["Tasbih"])
 st.subheader("Tasbih")
 st.dataframe(df,hide_index=True)
-tasbih = {st.selectbox("Select Tasbeeh Type", ["Type 1", "Type 2", "Type 3", "Type 4"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=6),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=7)]}
+tasbih = {st.selectbox("Select Tasbeeh Type", data["Tasbih"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=6),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=7)]}
 st.divider()    
 
 df = pd.DataFrame(data["Miswak"])
 st.subheader("Miswak")
 st.dataframe(df,hide_index=True)
-miswak = {st.selectbox("Select Miswak Type", ["Type 1", "Type 2", "Type 3", "Type 4"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=8),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=9)]}
+miswak = {st.selectbox("Select Miswak Type", data["Miswak"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=8),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=9)]}
 st.divider()    
 
 df = pd.DataFrame(data["Topi"])
 st.subheader("Topi")
 st.dataframe(df,hide_index=True)
-topi = {st.selectbox("Select Topi Type", ["Type 1", "Type 2", "Type 3", "Type 4"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=10),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=11)]}
+topi = {st.selectbox("Select Topi Type", data["Topi"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=10),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=11)]}
 st.divider()    
 
 df = pd.DataFrame(data["Mat"])
 st.subheader("Prayer Mat")
 st.dataframe(df,hide_index=True)
-mat = {st.selectbox("Select Prayer Mat Type", ["Type 1", "Type 2", "Type 3", "Type 4"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=12),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=13)]}
+mat = {st.selectbox("Select Prayer Mat Type", data["Mat"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=12),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=13)]}
+
+df = pd.DataFrame(data["Itar"])
+st.subheader("Prayer Mat")
+st.dataframe(df,hide_index=True)
+itar = {st.selectbox("Select Prayer Mat Type", data["Itar"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=12),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key=13)]}
 
   
 
 st.divider()    
 
 if st.button("Calculate Total Cost"):
-    total_cost = calculate_total_cost(dates, tasbih, miswak, topi, zamzam, mat)
+    total_cost = calculate_total_cost(dates, tasbih, miswak, topi, zamzam, mat, itar)
     st.success(f"Total Cost: Rs {total_cost} + Delivery Charges (as applicable)")
