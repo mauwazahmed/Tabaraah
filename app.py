c01224688b237de28a98bdbb2e4c2b7c1cf2bac6
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2025-04-12 23:02:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat, itar):
+def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam_b, zamzam_f, mat, itar):
     prices = {
-        "Dates": {"Ajwa": 6, "Kalmi": 6, "Sukri": 6, "Mabroom": 4},
-        "Tasbih" : {"Type 1": 10, "Type 2": 15, "Type 3": 17, "Type 4": 20},
-        "Miswak" : {"Type 1": 10, "Type 2": 15},
-        "Topi" : {"Type 1": 15, "Type 2": 17, "Type 3": 20, "Type 4": 25},
-        "ZamZam" : {"Pink 100ml":6, "Pink 60ml" : 5, "Green 60ml" : 2.5},
-        "Mat" : {"Type 1": 120, "Type 2": 150, "Type 3": 195, "Type 4": 250},
+        "Dates": {"Ajwa": 9, "Kalmi": 7, "Sukri": 5, "Rushdi": 3},
+        "Tasbih" : {"Type 1": 12, "Type 2": 17, "Type 3": 20, "Type 4": 25},
+        "Miswak" : {"Type 1": 12, "Type 2": 18},
+        "Topi" : {"Type 1": 20, "Type 2": 25, "Type 3": 30},
+        "ZamZam (Bottle Only)" : {"Pink 100ml":6, "Pink 60ml" : 5, "Green 60ml" : 2.5},
+        "ZamZam (Filled)" : {"Pink 100ml":40, "Pink 60ml" : 30, "Green 60ml" : 27},
+        "Mat" : {"Type 1": 150, "Type 2": 180, "Type 3": 220, "Type 4": 300},
         "Itar" : {"Type 1": 10, "Type 2": 15, "Type 3": 20}
     }
     
@@ -21,8 +22,10 @@ def calculate_total_cost(dates, tasbeeh, miswak, topi, zamzam, mat, itar):
         total_cost += prices["Miswak"][item] * detail[0] * detail[1]
     for item, detail in topi.items():
         total_cost += prices["Topi"][item] * detail[0] * detail[1]
-    for item, detail in zamzam.items():
-        total_cost += prices["ZamZam"][item] * detail[0] * detail[1]
+    for item, detail in zamzam_b.items():
+        total_cost += prices["ZamZam (Bottle Only)"][item] * detail[0] * detail[1]
+    for item, detail in zamzam_f.items():
+        total_cost += prices["ZamZam (Filled)"][item] * detail[0] * detail[1]
     for item, detail in mat.items():
         total_cost += prices["Mat"][item] * detail[0] * detail[1]
     for item, detail in itar.items():
@@ -79,7 +82,8 @@ st.divider()
 df = pd.DataFrame(data["ZamZam"])
 st.subheader("ZamZam")
 st.dataframe(df,hide_index=True)
-zamzam = {st.selectbox("Select Zamzam Bottle Quantity", data["ZamZam"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=2),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key = 3)]}
+zamzam_b = {st.selectbox("Select Zamzam Bottle Quantity", data["ZamZam"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=2),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key = 3)]}
+zamzam_f = {st.selectbox("Select Zamzam Bottle Quantity", data["ZamZam"]["Type"]): [st.number_input("Number of Units in One Packet", min_value=0, step=1, key=-2),st.number_input("Number of Packets Required", min_value=0, step=1, value=num_packets, key = -3)]}
 st.divider()    
 
 
@@ -122,5 +126,5 @@ itar = {st.selectbox("Select Prayer Mat Type", data["Itar"]["Type"]): [st.number
 st.divider()    
 
 if st.button("Calculate Total Cost"):
-    total_cost = calculate_total_cost(dates, tasbih, miswak, topi, zamzam, mat, itar)
-    st.success(f"Total Cost: Rs {total_cost} + Delivery Charges (as applicable). With GST: Rs. {total_cost*1.18}")
+    total_cost = calculate_total_cost(dates, tasbih, miswak, topi, zamzam_b, zamzam_f, mat, itar)
+    st.success(f"Total Cost: Rs {total_cost} + Delivery Charges (as applicable)")
